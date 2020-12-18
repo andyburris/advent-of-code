65abfcdee844b240918ff0f7984f92fec421199d
andb3 <andb3003@yahoo.com>
2020-12-18 01:05:36 -0500

    2020 Day 18 part 2 solve

--- src/com/andb/adventofcode/year2020/day18/Day18.kt
+++ src/com/andb/adventofcode/year2020/day18/Day18.kt
@@ -5,20 +5,22 @@ import java.io.File
 private val reader = File("src/com/andb/adventofcode/year2020/day18/input.txt").bufferedReader()
 private val testReader = File("src/com/andb/adventofcode/year2020/day18/test.txt").bufferedReader()
 
-fun main(){
-    partOne()
+fun main() {
+    //partOne()
+    partTwo()
 }
 
-private fun partOne(){
+private fun partOne() {
     val expressions = reader.readLines().map { it.toValues() }
-    println(expressions.fold(0L) { acc, list ->  acc + list.evaluate() })
+    println(expressions.fold(0L) { acc, list -> acc + list.evaluateBasic() })
 }
 
-private fun partTwo(){
-
+private fun partTwo() {
+    val expressions = reader.readLines().map { it.toValues() }
+    println(expressions.fold(0L) { acc, list -> acc + list.evaluateAdvanced() })
 }
 
-private fun test(){
+private fun test() {
 
 }
 
@@ -27,26 +29,24 @@ private sealed class Value {
         object Plus : Operator()
         object Times : Operator()
 
-        fun operate(left: Long, right: Long) = when(this) {
+        fun operate(left: Long, right: Long) = when (this) {
             Plus -> left + right
             Times -> left * right
         }
     }
+
     data class Number(val value: Long) : Value()
     data class Group(val values: List<Value> = emptyList(), val closed: Boolean = false) : Value()
 }
 
-//data class Operation(val left: Value, val operation: Value.Operator, val right: Value) : Value()
-
-
 private fun String.toValues(): List<Value> {
     return this.filterNot { it == ' ' }.fold(emptyList()) { acc, c ->
-        val nextValue = when {
-            c.toString().toLongOrNull() != null -> Value.Number(c.toString().toLong())
-            c == '+' -> Value.Operator.Plus
-            c == '*' -> Value.Operator.Times
-            c == '(' -> Value.Group()
-            c == ')' -> return@fold acc.closeMostNested()
+        val nextValue = when (c) {
+            in '0'..'9' -> Value.Number(c.toString().toLong())
+            '+' -> Value.Operator.Plus
+            '*' -> Value.Operator.Times
+            '(' -> Value.Group()
+            ')' -> return@fold acc.closeMostNested()
             else -> throw Error("Not supported ($c)")
         }
         acc.addToMostNested(nextValue)
@@ -55,21 +55,53 @@ private fun String.toValues(): List<Value> {
 
 private fun List<Value>.addToMostNested(value: Value): List<Value> {
     val lastValue = this.lastOrNull()
-    return if (lastValue is Value.Group && !lastValue.closed) this.dropLast(1) + lastValue.copy(values = lastValue.values.addToMostNested(value)) else this + value
+    return if (lastValue is Value.Group && !lastValue.closed) this.dropLast(1) + lastValue.copy(
+        values = lastValue.values.addToMostNested(
+            value
+        )
+    ) else this + value
 }
 
 private fun List<Value>.closeMostNested(): List<Value> {
     val lastValue = this.lastOrNull()
     if (lastValue !is Value.Group) throw Error("Can only close a group!")
     val nestedLast = lastValue.values.last()
-    return if (nestedLast is Value.Group && !nestedLast.closed) this.dropLast(1) + lastValue.copy(values = lastValue.values.closeMostNested()) else this.dropLast(1) + lastValue.copy(closed = true)
+    return if (nestedLast is Value.Group && !nestedLast.closed) this.dropLast(1) + lastValue.copy(values = lastValue.values.closeMostNested()) else this.dropLast(
+        1
+    ) + lastValue.copy(closed = true)
 }
 
-private fun List<Value>.evaluate(): Long = this.fold<Value, Pair<Long, Value.Operator>>(Pair(0L, Value.Operator.Plus)) { acc, value ->
-    when(value) {
-        is Value.Operator -> acc.copy(second = value)
-        is Value.Number -> acc.copy(first = acc.second.operate(acc.first, value.value))
-        is Value.Group -> acc.copy(first = acc.second.operate(acc.first, value.values.evaluate()))
-    }
-}.first
+private fun List<Value>.evaluateBasic(): Long =
+    this.fold<Value, Pair<Long, Value.Operator>>(Pair(0L, Value.Operator.Plus)) { acc, value ->
+        when (value) {
+            is Value.Operator -> acc.copy(second = value)
+            is Value.Number -> acc.copy(first = acc.second.operate(acc.first, value.value))
+            is Value.Group -> acc.copy(first = acc.second.operate(acc.first, value.values.evaluateBasic()))
+        }
+    }.first
 
+private fun List<Value>.evaluateAdvanced(): Long {
+    val grouped = this.groupByPriority()
+    println("this = $this")
+    println("grouped = $grouped")
+    return grouped.evaluateBasic()
+}
+
+private fun List<Value>.groupByPriority(): List<Value> {
+    val groupNested = this.map { if (it is Value.Group) it.copy(values = it.values.groupByPriority()) else it }
+    val newGrouped = mutableListOf(groupNested.first())
+    groupNested
+        .drop(1)
+        .chunked(2)
+        .map { (operator, value) ->
+            if (operator is Value.Operator.Plus) {
+                val left = newGrouped.removeAt(newGrouped.size - 1)
+                val group = listOf(left) + operator + value
+                newGrouped.add(Value.Group(group, true))
+            } else {
+                newGrouped.add(operator)
+                newGrouped.add(value)
+            }
+        }
+    return newGrouped
+}
