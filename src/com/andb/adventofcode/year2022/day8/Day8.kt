88b58a765e92b02a525c5a814c8b9801eaceb94b
andb3 <andb3003@yahoo.com>
2022-12-08 03:46:17 -0500

    2022 Day 8 raw

---
+++ src/com/andb/adventofcode/year2022/day8/Day8.kt
@@ -0,0 +1,69 @@
+package com.andb.adventofcode.year2022.day8
+
+import com.andb.adventofcode.columns
+import com.andb.adventofcode.year2020.common.Quad
+import com.andb.adventofcode.year2020.common.pairCombinations
+import java.io.File
+
+private val reader = File("src/com/andb/adventofcode/year2022/day8/input.txt").bufferedReader()
+private val testReader = File("src/com/andb/adventofcode/year2022/day8/test.txt").bufferedReader()
+
+fun main() {
+    partTwo()
+}
+
+fun partOne() {
+    val trees: List<List<Int>> = reader.readLines().map { line -> line.toList().map{ it.digitToInt() } }
+    val allCoordinates = (trees.indices).flatMap { row -> trees[0].indices.map { col -> row to col }}
+    println(allCoordinates)
+
+    val asColumns = trees.columns()
+
+    val visibleTrees = allCoordinates.filter { (row, col) ->
+        val isVisible = trees[row].take(col + 1).isVisible() //left
+                || asColumns[col].take(row + 1).isVisible() //top
+                || trees[row].drop(col).reversed().isVisible() //right
+                || asColumns[col].drop(row).reversed().isVisible() //bottom
+        isVisible
+    }
+    println(visibleTrees.size)
+}
+
+fun partTwo() {
+    val trees: List<List<Int>> = reader.readLines().map { line -> line.toList().map{ it.digitToInt() } }
+    val scenicScores: List<List<Int>> = trees.sightlines().map { row ->
+        row.map { it.score() }
+    }
+    println(scenicScores.flatten().maxOf { it })
+}
+
+fun test() {
+
+}
+
+private fun <T : Comparable<T>> List<T>.isSorted(): Boolean = this.windowed(2).all { it[0] < it[1] }
+private fun List<Int>.isVisible(): Boolean = this.last() > (this.dropLast(1).maxOfOrNull { it } ?: -1)
+
+private fun List<List<Int>>.sightlines(): List<List<Sightlines>> {
+    val asColumns = this.columns()
+    return this.mapIndexed { row, treeRow ->
+        treeRow.mapIndexed { col, treeHeight ->
+            Sightlines(
+                left = this[row].take(col + 1),
+                top = asColumns[col].take(row + 1),
+                right = this[row].drop(col).reversed(),
+                bottom = asColumns[col].drop(row).reversed(),
+            )
+        }
+    }
+}
+
+private data class Sightlines(
+    val top: List<Int>,
+    val left: List<Int>,
+    val bottom: List<Int>,
+    val right: List<Int>,
+)
+
+private fun Sightlines.score() = listOf(top, left, bottom, right).fold(1) { acc, line -> acc * line.amountVisible() }
+private fun List<Int>.amountVisible() = (this.dropLast(1).takeLastWhile { it < this.last() }.size + 1).coerceAtMost(this.size - 1)
