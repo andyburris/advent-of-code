3d6ee5661dcb983c0f446a7d2857d5fb4d3626f5
andb3 <andb3003@yahoo.com>
2022-12-11 23:57:35 -0500

    2022 Day 11 cleanup

--- src/com/andb/adventofcode/year2022/day11/Day11.kt
+++ src/com/andb/adventofcode/year2022/day11/Day11.kt
@@ -3,7 +3,6 @@ package com.andb.adventofcode.year2022.day11
 import com.andb.adventofcode.year2020.common.splitToGroups
 import java.io.File
 import java.lang.Error
-import java.math.BigInteger
 
 private val reader = File("src/com/andb/adventofcode/year2022/day11/input.txt").bufferedReader()
 private val testReader = File("src/com/andb/adventofcode/year2022/day11/test.txt").bufferedReader()
@@ -22,45 +21,38 @@ private fun partOne(){
             val currentMonkey = accMonkeys[currentMonkeyIndex]
             val allThrown = currentMonkey.items.fold(accMonkeys) { accMonkeys, item ->
                 val inspectionWorryLevel = currentMonkey.operation(item)
-                val reliefWorryLevel = inspectionWorryLevel / (3.toBigInteger())
+                val reliefWorryLevel = inspectionWorryLevel / 3
                 val throwTo = if (currentMonkey.test.testFunction(reliefWorryLevel)) currentMonkey.test.throwToIfTrue else currentMonkey.test.throwToIfFalse
                 accMonkeys.mapIndexed { index, monkey -> if (index == throwTo) monkey.copy(items = monkey.items + reliefWorryLevel) else monkey }
             }
-            val updatedInspections = allThrown.mapIndexed { index, monkey -> if (index == currentMonkeyIndex) monkey.copy(items = emptyList(), totalInspections = monkey.totalInspections + currentMonkey.items.size.toBigInteger()) else monkey }
+            val updatedInspections = allThrown.mapIndexed { index, monkey -> if (index == currentMonkeyIndex) monkey.copy(items = emptyList(), totalInspections = monkey.totalInspections + currentMonkey.items.size.toInt()) else monkey }
             updatedInspections
         }
         finalMonkeys
     }
-    println(afterAllRounds)
-    println(afterAllRounds.map { it.totalInspections })
     println(afterAllRounds.sortedBy { it.totalInspections }.takeLast(2).let { it[0].totalInspections * it[1].totalInspections })
 }
 
 private fun partTwo(){
     val monkeys = reader.readLines().splitToGroups { it.isBlank() }.map { it.toMonkey() }
-    val maxModulo = monkeys.fold(1) { acc, monkey -> acc * monkey.test.divisibleBy }.toBigInteger()
+    val maxModulo = monkeys.fold(1) { acc, monkey -> acc * monkey.test.divisibleBy }.toInt()
     println(monkeys)
     val numRounds = 10000L
-    val inspectionRounds = listOf(1, 20, 1000, 2000).map { it.toLong() - 1 }
     val afterAllRounds = (0 until numRounds).fold(monkeys) { roundMonkeys, roundIndex ->
         val finalMonkeys = roundMonkeys.indices.fold(roundMonkeys) { accMonkeys, currentMonkeyIndex ->
             val currentMonkey = accMonkeys[currentMonkeyIndex]
             val allThrown = currentMonkey.items.fold(accMonkeys) { accMonkeys, item ->
                 val inspectionWorryLevel = currentMonkey.operation(item)
-                //val reliefWorryLevel = inspectionWorryLevel / 3
-                val moddedWorryLevel = inspectionWorryLevel.remainder(maxModulo)
+                val moddedWorryLevel = inspectionWorryLevel % maxModulo
                 val throwTo = if (currentMonkey.test.testFunction(moddedWorryLevel)) currentMonkey.test.throwToIfTrue else currentMonkey.test.throwToIfFalse
                 accMonkeys.mapIndexed { index, monkey -> if (index == throwTo) monkey.copy(items = monkey.items + moddedWorryLevel) else monkey }
             }
-            val updatedInspections = allThrown.mapIndexed { index, monkey -> if (index == currentMonkeyIndex) monkey.copy(items = emptyList(), totalInspections = monkey.totalInspections + currentMonkey.items.size.toBigInteger()) else monkey }
+            val updatedInspections = allThrown.mapIndexed { index, monkey -> if (index == currentMonkeyIndex) monkey.copy(items = emptyList(), totalInspections = monkey.totalInspections + currentMonkey.items.size.toInt()) else monkey }
             updatedInspections
         }
-        if (roundIndex in inspectionRounds) println("After Round ${roundIndex + 1}: ${finalMonkeys.mapIndexed { index, monkey -> "Monkey $index inspected items ${monkey.totalInspections} times" }.joinToString(", ")}")
         finalMonkeys
     }
-    println(afterAllRounds)
-    println(afterAllRounds.map { it.totalInspections })
-    println(afterAllRounds.sortedBy { it.totalInspections }.takeLast(2).let { it[0].totalInspections * it[1].totalInspections })
+    println(afterAllRounds.sortedBy { it.totalInspections }.takeLast(2).let { it[0].totalInspections.toLong() * it[1].totalInspections })
 }
 
 private fun test(){
@@ -68,17 +60,17 @@ private fun test(){
 }
 
 private data class Monkey(
-    val items: List<BigInteger>,
-    val operation: (BigInteger) -> BigInteger,
+    val items: List<Long>,
+    val operation: (Long) -> Long,
     val test: Test,
-    val totalInspections: BigInteger = BigInteger.ZERO
+    val totalInspections: Int = 0
 ) {
     data class Test(
         val divisibleBy: Int,
         val throwToIfTrue: Int,
         val throwToIfFalse: Int,
     ) {
-        val testFunction: (BigInteger) -> Boolean = { it.remainder(divisibleBy.toBigInteger()) == BigInteger.ZERO }
+        val testFunction: (Long) -> Boolean = { it % divisibleBy == 0L }
     }
 }
 
@@ -87,8 +79,8 @@ private fun List<String>.toMonkey(): Monkey {
     val rawOperation: String = this[2].trim()
     val rawTest: List<String> = this.drop(3)
 
-    val startingItems = rawStartingItems.removePrefix("Starting items: ").split(", ").map { it.toBigInteger() }
-    val operation: (BigInteger) -> BigInteger = rawOperation.removePrefix("Operation: new = ").split(" ").let {
+    val startingItems = rawStartingItems.removePrefix("Starting items: ").split(", ").map { it.toLong() }
+    val operation: (Long) -> Long = rawOperation.removePrefix("Operation: new = ").split(" ").let {
         val term1 = it[0].toTerm()
         val term2 = it[2].toTerm()
 
@@ -104,12 +96,12 @@ private fun List<String>.toMonkey(): Monkey {
 
 private sealed class Term {
     object Old : Term()
-    data class Number(val value: BigInteger) : Term()
+    data class Number(val value: Long) : Term()
 }
 
 private fun String.toTerm() = when {
     this == "old" -> Term.Old
-    this.toIntOrNull() != null -> Term.Number(this.toBigInteger())
+    this.toIntOrNull() != null -> Term.Number(this.toLong())
     else -> throw Error("$this is an invalid Term")
 }
 
